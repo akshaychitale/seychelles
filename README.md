9d1a81f8880a547ffa8ce9588c7c5e9695fbaa9e
akshaychitale <akshaychitale3@gmail.com>
2018-02-28 03:23:13 -0600

    First Commit

---
+++ README.md
@@ -0,0 +1,80 @@
+# Seychelles Flag Generator
+
+A Python script to convert an image of a flag (or really any image) into one in the style of the flag of the Seychelles, or the other way around.
+
+Made for [/r/vexillology](https://www.reddit.com/r/vexillology/) on Reddit.
+
+## How to Use
+
+### Prerequisites
+
+In order to run the Python script, you will, of course, have to have Python installed. The scripts was tested on macOS with both Python 2.7.13 and Python 3.6.4, so either Python 3 or Python 2.7 should work fine.
+
+In addition, you will need to have the Python Imaging Library (PIL) installed. This can be installed with pip (or with pip3):
+
+```
+$ pip install Pillow
+```
+
+### Running the Script
+
+Download the file `seychelles.py` and run it with Python, passing in the name of the file to process:
+
+```
+$ python seychelles.py image_in.png
+```
+
+For information about all of the options availible, such as setting the output image size and doing an inverse seychelles operation, use the help option:
+
+```
+$ python seychelles.py -h
+usage: seychelles.py [-h] [-i] [-s SIZE SIZE] [-n NAME] [-e EXT] [-d] [-v]
+                     image_in
+
+positional arguments:
+  image_in              Image file to process
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -i, --inverse         Do inverse seychelles
+  -s SIZE SIZE, --size SIZE SIZE
+                        Output image width and height
+  -n NAME, --name NAME  Output file name
+  -e EXT, --ext EXT     Output file extension
+  -d, --display         Display output instead of saving to file
+  -v, --verbose         Display progress while processing
+```
+
+### Advanced Use
+
+You can also write your own script and just import the Seychelles class to do more complicated things than the main script allows for. The Seychelles class has the following methods:
+
+* __init__ - Creates a new Seychelles object
+* seychelles - Performs the forward Seychelles flag transformation
+* inverse_seychelles - Performs the reverse transformation back to a regular flag
+* save - Saves the output image to a file
+* show - Displays the output image in the default image viewer
+
+Run `python seychelles.py -h` to see what the arguments to the functions do. The main script just passes the command line arguments to these arguments in the Seychelles class's methods.
+
+Feel free to modify any of the code, like the angle mapping or the algorithm itself, to make your own flag generators!
+
+## Technical Details
+
+The transformation that is performed is a mapping from rectangular coordinates in the input to polar coordinates in the output. The X coordinate corresponds with the radius in the output, and the Y coordinate with the angle in the output. Note that pixels here are referenced with the bottom left being (0,0)
+
+For each pixel of the output, the relative radius and angle of that pixel are computed. The relative radius is the fraction of the radius to the edge of the flag that the pixel is at. The angle is counterclockwise relative to the positive X axis.
+
+There is an angle mapping that ensures that the middle of the input flag, Y = input height/2, corresponds with the diagonal of the output flag. This is ensured by finding the parabola that contains the points (0,0), (output diagonal, pi/4), and (pi/2, pi/2) to generate a function of input angle vs output angle.
+
+Finally, the output pixel is assigned the color from the corresponding input pixel. This pixel is at the same horizontal position as the radius ratio - for example, an output pixel 1/3 of the way to the edge of the flag radually would correspond to an input pixel 1/3 of the way to the edge of the flag horizontally. The Y coordinate is the mapped angle, scaled such that 0 corresonds with Y = 0 and pi/2 corresponds with the top fo the flag, Y = input height.
+
+The inverse operation does the same thing, but in reverse. For example, the angle mapping is a square root function in the inverse opertion instead of a parabola.
+
+## Author
+
+**Akshay Chitale**
+
+## Disclaimer
+
+I made this script for fun - use, modify, and redistribute it however you wish. However, note that I make no guarantee, claim, or promise of any sort as to the functionality of this code. Use part or all of it for serious projects at your own risk.
---
+++ seychelles.py
@@ -0,0 +1,192 @@
+from __future__ import print_function
+from PIL import Image
+import argparse
+import math
+import os
+
+# For Python 3
+try:
+	xrange
+except NameError:
+	xrange = range
+
+class Seychelles:
+	def __init__(self, name_in, size_out=None, name_out=None, ext_out=None):
+		# Set up input
+		self.name_in, self.ext_in = os.path.splitext(name_in)
+		self.img_raw = Image.open(name_in)
+		self.img_raw = self.img_raw.convert('RGB')
+		self.size_in = self.img_raw.size
+		# Flip so that colors are measured from top left
+		self.img_in = self.img_raw.transpose(Image.FLIP_TOP_BOTTOM)
+
+		# Set up output
+		self.size_out = size_out if size_out else self.size_in
+		self.name_out = name_out if name_out else self.name_in + '_out'
+		self.ext_out = '.' + ext_out if ext_out else self.ext_in
+		self.img_out = Image.new('RGB', self.size_out)
+		self.pixels_out = self.img_out.load()
+
+		# Set up image to print
+		self.img_print = None
+
+	def _angle_transfer(self, diagonal, seychelles):
+		# Define transfer curve as a parabola
+		x1, y1 = 0, 0
+		x2, y2 = (diagonal, math.pi/4)
+		x3, y3 = math.pi/2, math.pi/2
+		# Below code from http://chris35wills.github.io/parabola_python/
+		denom = (x1-x2) * (x1-x3) * (x2-x3);
+		A     = (x3 * (y2-y1) + x2 * (y1-y3) + x1 * (y3-y2)) / denom;
+		B     = (x3*x3 * (y1-y2) + x2*x2 * (y3-y1) + x1*x1 * (y2-y3)) / denom;
+		# C = 0 guaranteed because parabola goes through (0,0)
+		if seychelles:
+			# Going forward, use parabola
+			return (lambda x: A*x*x + B*x)
+		else:
+			# Inverse, solve parabola y=ax^2+bx
+			# Watch out for squares, where diagonal is pi/4 so A=0
+			if abs(diagonal - math.pi/4) < 1E-9:
+				return (lambda x: x)
+			else:
+				return (lambda x: (-1*B + math.sqrt(B*B - 4*A*(-x)))/(2*A))
+
+	def seychelles(self, verbose=False):
+		# Diagonal angle of output image
+		out_diagonal = math.atan2(self.size_out[1], self.size_out[0])
+		angle_transfer = self._angle_transfer(out_diagonal, True)
+		# Find output color for each output pixel
+		if verbose:
+			print(' Progress:   0%', end='\r')
+		for x in xrange(self.size_out[0]):
+			if verbose:
+				print('\r Progress: ' + str(int(100*x/self.size_out[0])).rjust(3) + '%', end='\r')
+			for y in xrange(self.size_out[1]):
+				# First, get the angle
+				out_angle = math.atan2(y, x)
+
+				# Then, follow the vector to the end of the flag
+				out_x, out_y = x, y
+				if x == 0 and y == 0:
+					out_x, out_y = 1.0, 1.0
+				elif out_angle < out_diagonal:
+					# Scale by x
+					out_x *= self.size_out[0]*1.0/x
+					out_y *= self.size_out[0]*1.0/x
+				else:
+					# Scale by y
+					out_x *= self.size_out[1]*1.0/y
+					out_y *= self.size_out[1]*1.0/y
+
+				# Get ratio of point radius to full radius
+				point_rad = math.sqrt(x*x + y*y)
+				out_rad = math.sqrt(out_x*out_x + out_y*out_y)
+				rad_ratio = point_rad / out_rad
+
+				# Coordinates on the input are:
+				# x = radius, scaled by width of input
+				in_x = rad_ratio * self.size_in[0]
+				# y = angle, scaled by height of input
+				in_y = angle_transfer(out_angle) * self.size_in[1] * 2.0 / math.pi
+
+				# Ensure coordinates are within range
+				in_x_int = int(round(in_x))
+				if in_x_int < 0:
+					in_x_int = 0
+				elif in_x_int >= self.size_in[0]:
+					in_x_int = self.size_in[0] - 1
+				in_y_int = int(round(in_y))
+				if in_y_int < 0:
+					in_y_int = 0
+				elif in_y_int >= self.size_in[1]:
+					in_y_int = self.size_in[1] - 1
+
+				# Assign input color to output color
+				self.pixels_out[x,y] = self.img_in.getpixel((in_x_int, in_y_int))
+		if verbose:
+				print('\r Progress: 100%')
+		# Flip so that seychelles is from bottom left
+		self.img_print = self.img_out.transpose(Image.FLIP_TOP_BOTTOM)
+
+
+	def inverse_seychelles(self, verbose=False):
+		# Diagonal angle of input image
+		in_diagonal = math.atan2(self.size_in[1], self.size_in[0])
+		angle_transfer = self._angle_transfer(in_diagonal, False)
+		# Find output color for each output pixel
+		if verbose:
+			print(' Progress:   0%', end='\r')
+		for x in xrange(self.size_out[0]):
+			if verbose:
+				print('\r Progress: ' + str(int(100*x/self.size_out[0])).rjust(3) + '%', end='\r')
+			for y in xrange(self.size_out[1]):
+				# First, get the angle
+				in_angle = angle_transfer(y * math.pi / 2.0 / self.size_out[1])
+
+				# Get ratio of point to full width
+				rad_ratio = x * 1.0 / self.size_out[0]
+
+				# Then, follow the vector to the end of the flag
+				if in_angle < in_diagonal:
+					# Find by x
+					in_x = self.size_in[0] * 1.0
+					in_y = in_x * math.tan(in_angle)
+				else:
+					# Find by y
+					in_y = self.size_in[1] * 1.0
+					in_x = in_y / math.tan(in_angle)
+				# Scale by radius ratio
+				in_x, in_y = rad_ratio*in_x, rad_ratio*in_y
+
+				# Ensure coordinates are within range
+				in_x_int = int(round(in_x))
+				if in_x_int < 0:
+					in_x_int = 0
+				elif in_x_int >= self.size_in[0]:
+					in_x_int = self.size_in[0] - 1
+				in_y_int = int(round(in_y))
+				if in_y_int < 0:
+					in_y_int = 0
+				elif in_y_int >= self.size_in[1]:
+					in_y_int = self.size_in[1] - 1
+
+				# Assign input color to output color
+				self.pixels_out[x,y] = self.img_in.getpixel((in_x_int, in_y_int))
+		if verbose:
+				print('\r Progress: 100%')
+		# Flip so that seychelles is from bottom left
+		self.img_print = self.img_out.transpose(Image.FLIP_TOP_BOTTOM)
+
+	def save(self, name_out=None, ext_out=None):
+		if self.img_print is None: raise Exception('No processing done yet')
+		name = name_out if name_out else self.name_out
+		ext = '.' + ext_out if ext_out else self.ext_out
+		self.img_print.save(name + ext)
+
+	def show(self):
+		if self.img_print is None: raise Exception('No processing done yet')
+		self.img_print.show()	
+
+if __name__ == "__main__":
+	# Parse args
+	parser = argparse.ArgumentParser()
+	parser.add_argument('image_in', type=str, help='Image file to process')
+	parser.add_argument('-i', '--inverse', action='store_true', default=False, help='Do inverse seychelles')
+	parser.add_argument('-s', '--size', type=int, nargs=2, default=None, help='Output image width and height')
+	parser.add_argument('-n', '--name', type=str, default=None, help='Output file name')
+	parser.add_argument('-e', '--ext', type=str, default=None, help='Output file extension')
+	parser.add_argument('-d', '--display', action='store_true', default=False, help='Display output instead of saving to file')
+	parser.add_argument('-v', '--verbose', action='store_true', default=False, help='Display progress while processing')
+	args = parser.parse_args()
+
+	# Run Seychelles
+	s = Seychelles(args.image_in, size_out=args.size, name_out=args.name, ext_out=args.ext)
+	if(args.inverse):
+		s.inverse_seychelles(verbose=args.verbose)
+	else:
+		s.seychelles(verbose=args.verbose)
+	if(args.display):
+		s.show()
+	else:
+		s.save()
+
